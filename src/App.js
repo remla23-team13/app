6045861df48df94f22713e376c1b512d8439085b
GasparinoRocha <gasparsantosrocha@gmail.com>
2023-05-11 16:30:30 +0200

    Imported private lib

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 import { Body } from './body/Body';
 import { Header } from './header/Header';
+import { VersionUtil } from '@remla23-team13/remla-lib'
 
 function App() {
+  console.log("Using version " + VersionUtil.getVersion() + " of the remla-lib.");
+
   return (
     <div className="App">
       <div className='Wrapper'>
