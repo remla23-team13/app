e050ff394001608dad7fa6c3ebe44ae9743628b6
GasparinoRocha <gasparsantosrocha@gmail.com>
2023-05-17 18:11:45 +0200

    Added wrong sentiment button and model service request

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import './App.css';
 import { Body } from './body/Body';
 import { Header } from './header/Header';
 import { VersionUtil } from '@remla23-team13/remla-lib'
+import { useEffect } from 'react';
 
 function App() {
-  console.log("Using version " + VersionUtil.getVersion() + " of the remla-lib.");
+
+  useEffect(()=>{
+    console.log("Using version " + VersionUtil.getVersion() + " of the remla-lib.");
+  }, [])
 
   return (
     <div className="App">
--- src/body/Body.js
+++ src/body/Body.js
@@ -6,15 +6,18 @@ export function Body() {
 
     const [review, setReview] = useState('');
     const [sentiment, setSentiment] = useState('');
-    const [disabled, setDisabled] = useState(true);
+    const [analyseDisabled, setAnalysedDisabled] = useState(true);
+    const [wrongSentimentDisabled, setWrongSentimentDisabled] = useState(false);
+    const [afterAnalysis, setAfterAnalysis] = useState(false);
 
     const handleReviewChange = event => {
         const newReview = event.target.value;
         if (newReview === "") {
-            setSentiment("");
-            setDisabled(true);
+            setAnalysedDisabled(true);
         }
-        else if (disabled) setDisabled(false);
+        else if (analyseDisabled) setAnalysedDisabled(false);
+        setSentiment("");
+        setAfterAnalysis(false);
         setReview(newReview);
     };
 
@@ -25,10 +28,15 @@ export function Body() {
         else if (res.prediction === "Negative") {
             setSentiment(String.fromCodePoint(128577));
         }
+
+        
+        setAnalysedDisabled(true);
+        setWrongSentimentDisabled(false);
+        setAfterAnalysis(true);
     } 
 
     const analyzeReview = useCallback(() => {
-        fetch(process.env.REACT_APP_API_URL, {
+        fetch(process.env.MODEL_S_SERVICE_PATH + "predict", {
             method: 'POST',
             headers: {
               'Accept': 'application/json',
@@ -47,18 +55,26 @@ export function Body() {
          });
     }, [review]);
 
-    /*const analyzeReview = () => {
-        handleResponse({
-            sentiment: "happy"
-        });
-    }*/
+    // const analyzeReview = () => {
+    //     handleResponse({
+    //         prediction: "Positive"
+    //     });
+    // }
+
+    const incrementNWrongSentiments = () => {
+        fetch(process.env.MODEL_S_SERVICE_PATH + "wrong", {
+            method: 'POST'
+            })
+        setWrongSentimentDisabled(true);
+    }
     
     return (
         <div className="Body">
             <textarea className='ReviewTextArea' value={review} onChange={handleReviewChange}></textarea>
             <div className='Analysis'>
-                <button className='AnalyseReviewButton' onClick={analyzeReview} disabled={disabled}>Check Sentiment</button>
+                <button className='AnalyseReviewButton' onClick={analyzeReview} disabled={analyseDisabled}>Check Sentiment</button>
                 <Emoji className='SentimentEmoji' symbol={sentiment} />
+                { afterAnalysis && <button className='WrongSentimentButton' onClick={incrementNWrongSentiments} disabled={wrongSentimentDisabled} >Wrong Sentiment</button> }
             </div>
         </div>
     )
